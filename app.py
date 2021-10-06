9472dd01a8272f10698259d6001d611f7e4b0ad5
Serhii Abarovskyi <aserhii@protonmail.com>
2021-10-06 21:32:27 +0300

    WIP

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify, request
+
+# data = requests.post("http://192.168.0.106:5000/", data=json.dumps({'k': 'v'}))
 
 app = Flask(__name__)
 
+data = []
 
-@app.route("/")
-def hello_world():
-    return "Hello, there!"
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+
+    if request.method == "POST":
+        msg = json.loads(request.data)
+        data.append()
+        return jsonify(msg)
+    else:
+        return jsonify(data)
