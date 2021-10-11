6dbf08570c0d3dad9f033a617739b5e40b26aa82
Serhii Abarovskyi <aserhii@protonmail.com>
2021-10-11 16:10:22 +0300

    Add sec2

---
+++ secondary2/app.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Flask, jsonify, request
+
+# r = requests.post("http://localhost:10000/", data=json.dumps({'k': 'v'}))
+# r = requests.get("http://localhost:10000/")
+
+app = Flask(__name__)
+
+data = []
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+
+    if request.method == "POST":
+        msg = json.loads(request.data)
+        data.append(msg)
+        return jsonify(msg)
+    else:
+        return jsonify(data)
